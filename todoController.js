57615b5c3fbea89aae61d1fe2fa0c68357d8dc02
pratyayj <pratyay.jaidev@gmail.com>
2018-12-30 19:31:08 +0800

    Fix retrieve controller 2

--- todoController.js
+++ todoController.js
@@ -55,9 +55,11 @@ exports.retrieve = function (req, res) {
 */
 
 router.retrieve = function (req, res) {
+    Todo.get((req, res) => {
     Todo.findById(req.params.todoId, (err, todos) => {
         res.json(todos)
     });  
+});
 };
 
 
